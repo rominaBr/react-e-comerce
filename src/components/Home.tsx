67cfd68ea5660f3a116e9f41f1f43d78f12c99ed
rominabr <romina.brutti@gmail.com>
2023-08-04 21:03:54 -0300

    Etapa-5

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,18 @@
 import "./styles.css"
+import font from "../assets/ecommerce.jpg"
+import { Link } from "react-router-dom"
 
 function Home(){
     return(
         <>
-            <div className="container">Home</div>
-        
+            <div style={{backgroundImage: `url(${font})`,
+                                                backgroundRepeat: "no-repeat",
+                                                backgroundSize: "cover",
+                                                width: "100%",
+                                                height: "100vh"}}>
+                <div className="button-buy"><Link to="/products" className="link">Comenzar Compra</Link></div>
+            </div>
+            
         </>
         
     )
